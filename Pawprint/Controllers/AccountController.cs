7bd3fd47826f9a508ae9eeaa3d4f75a1802bf978
BrettMerri <the.b.merri@gmail.com>
2017-05-19 16:41:30 -0400

    Added firstname and lastname to the registration form

--- Pawprint/Controllers/AccountController.cs
+++ Pawprint/Controllers/AccountController.cs
@@ -151,7 +151,11 @@ namespace Pawprint.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.Email,
+                                                 Email = model.Email,
+                                                 FirstName = model.FirstName,
+                                                 LastName = model.LastName };
+
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
