44e65595a5c037b955eeaaeed4187cb7d312e165
BrettMerri <the.b.merri@gmail.com>
2017-05-19 14:28:01 -0400

    Initial commit

---
+++ Pawprint/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Pawprint.Startup))]
+namespace Pawprint
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
