6d9834948f7d172499c2aea1dea08f85f941c7e6
BrettMerri <the.b.merri@gmail.com>
2017-05-21 11:50:02 -0400

    Added username field when registering account. Changed _LoginPartial to a dropdown menu containing Profile, Manage, and Logout

--- Pawprint/Controllers/AccountController.cs
+++ Pawprint/Controllers/AccountController.cs
@@ -151,7 +151,7 @@ namespace Pawprint.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email,
+                var user = new ApplicationUser { UserName = model.Username,
                                                  Email = model.Email,
                                                  FirstName = model.FirstName,
                                                  LastName = model.LastName };
--- Pawprint/Models/AccountViewModels.cs
+++ Pawprint/Models/AccountViewModels.cs
@@ -73,6 +73,10 @@ namespace Pawprint.Models
         public string LastName { get; set; }
 
         [Required]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
