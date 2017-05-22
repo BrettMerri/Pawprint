dc6c6c41bcbdf4d54d26ffe6430322e9025b565b
BrettMerri <the.b.merri@gmail.com>
2017-05-22 10:47:52 -0400

    Added user profile

---
+++ Pawprint/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Pawprint.Models;
+
+namespace Pawprint.Controllers
+{
+    public class UserController : Controller
+    {
+        // /User/Profile/[username]
+        public ActionResult Profile(string Username)
+        {
+            PawprintEntities DB = new PawprintEntities();
+            AspNetUser UserProfile = DB.AspNetUsers.SingleOrDefault(x => x.UserName == Username);
+
+            if (UserProfile == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            List<Pet> PetList = DB.Pets.Where(x => x.OwnerID == UserProfile.ID).ToList();
+
+            ViewBag.PetList = PetList;
+
+            return View(UserProfile);
+        }
+    }
+}
