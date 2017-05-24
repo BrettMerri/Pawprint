d6c8d76c27ca65cecad5ce55ab9d09931e43d975
BrettMerri <the.b.merri@gmail.com>
2017-05-24 11:24:35 -0400

    Added pet information to the pet profile. Pushing all files

--- Pawprint/Models/Pawprint.Designer.cs
+++ Pawprint/Models/Pawprint.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\abe\Documents\Visual Studio 2015\Projects\Pawprint\Pawprint\Models\PawPrint.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Brett\Desktop\Projects\Pawprint\Pawprint\Models\Pawprint.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
